a5a593c284c4fc3912d2ac8d7de4994fd4be8988
Jissnu07 <63947849+Jissnu07@users.noreply.github.com>
2021-08-29 19:43:40 +0530

    Update gesture.py

--- gesture.py
+++ gesture.py
@@ -1,112 +1,60 @@
-# Imports
-import numpy as np
 import cv2
+import numpy as np
 import math
+cap = cv2.VideoCapture(0)
+while(cap.isOpened()):
+    ret, img = cap.read()
+    cv2.rectangle(img, (300,300), (100,100), (0,255,0),0)
+    crop_img = img[100:300, 100:300]
+    grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+    value = (35, 35)
+    blurred = cv2.GaussianBlur(grey, value, 0)
+    _, thresh1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    cv2.imshow('Thresholded', thresh1)
+    contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-# Open Camera
-capture = cv2.VideoCapture(0)
+    cnt = max(contours, key = lambda x: cv2.contourArea(x))
+    x, y, w, h = cv2.boundingRect(cnt)
+    cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
+    hull = cv2.convexHull(cnt)
 
-while capture.isOpened():
+    drawing = np.zeros(crop_img.shape,np.uint8)
+    cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
+    cv2.drawContours(drawing, [hull], 0,(0, 0, 255), 0)
+    hull = cv2.convexHull(cnt, returnPoints=False)
+    defects = cv2.convexityDefects(cnt, hull)
+    count_defects = 0
+    cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
+    for i in range(defects.shape[0]):
+        s,e,f,d = defects[i,0]
+        start = tuple(cnt[s][0])
+        end = tuple(cnt[e][0])
+        far = tuple(cnt[f][0])
 
-    # Capture frames from the camera
-    ret, frame = capture.read()
+        a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+        b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+        c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
 
-    # Get hand data from the rectangle sub window
-    cv2.rectangle(frame, (100, 100), (300, 300), (0, 255, 0), 0)
-    crop_image = frame[100:300, 100:300]
+        angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
 
-    # Apply Gaussian blur
-    blur = cv2.GaussianBlur(crop_image, (3, 3), 0)
-
-    # Change color-space from BGR -> HSV
-    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-
-    # Create a binary image with where white will be skin colors and rest is black
-    mask2 = cv2.inRange(hsv, np.array([2, 0, 0]), np.array([20, 255, 255]))
-
-    # Kernel for morphological transformation
-    kernel = np.ones((5, 5))
-
-    # Apply morphological transformations to filter out the background noise
-    dilation = cv2.dilate(mask2, kernel, iterations=1)
-    erosion = cv2.erode(dilation, kernel, iterations=1)
-
-    # Apply Gaussian Blur and Threshold
-    filtered = cv2.GaussianBlur(erosion, (3, 3), 0)
-    ret, thresh = cv2.threshold(filtered, 127, 255, 0)
-
-    # Show threshold image
-    cv2.imshow("Thresholded", thresh)
-
-    # Find contours
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    try:
-        # Find contour with maximum area
-        contour = max(contours, key=lambda x: cv2.contourArea(x))
-
-        # Create bounding rectangle around the contour
-        x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(crop_image, (x, y), (x + w, y + h), (0, 0, 255), 0)
-
-        # Find convex hull
-        hull = cv2.convexHull(contour)
-
-        # Draw contour
-        drawing = np.zeros(crop_image.shape, np.uint8)
-        cv2.drawContours(drawing, [contour], -1, (0, 255, 0), 0)
-        cv2.drawContours(drawing, [hull], -1, (0, 0, 255), 0)
-
-        # Find convexity defects
-        hull = cv2.convexHull(contour, returnPoints=False)
-        defects = cv2.convexityDefects(contour, hull)
-
-        # Use cosine rule to find angle of the far point from the start and end point i.e. the convex points (the finger
-        # tips) for all defects
-        count_defects = 0
-
-        for i in range(defects.shape[0]):
-            s, e, f, d = defects[i, 0]
-            start = tuple(contour[s][0])
-            end = tuple(contour[e][0])
-            far = tuple(contour[f][0])
-
-            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-            angle = (math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 180) / 3.14
-
-            # if angle > 90 draw a circle at the far point
-            if angle <= 90:
-                count_defects += 1
-                cv2.circle(crop_image, far, 1, [0, 0, 255], -1)
-
-            cv2.line(crop_image, start, end, [0, 255, 0], 2)
-
-        # Print number of fingers
-        if count_defects == 0:
-            cv2.putText(frame, "ONE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255),2)
-        elif count_defects == 1:
-            cv2.putText(frame, "TWO", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
-        elif count_defects == 2:
-            cv2.putText(frame, "THREE", (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
-        elif count_defects == 3:
-            cv2.putText(frame, "FOUR", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
-        elif count_defects == 4:
-            cv2.putText(frame, "FIVE", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
-        else:
-            pass
-    except:
-        pass
-
-    # Show required images
-    cv2.imshow("Gesture", frame)
-    all_image = np.hstack((drawing, crop_image))
-    cv2.imshow('Contours', all_image)
-
-    # Close the camera if 'q' is pressed
-    if cv2.waitKey(1) == ord('q'):
+        if angle <= 90:
+            count_defects += 1
+            cv2.circle(crop_img, far, 1, [0,0,255], -1)
+        cv2.line(crop_img,start, end, [0,255,0], 2)
+    if count_defects == 1:
+        cv2.putText(img,"2", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
+    elif count_defects == 2:
+        str = "3"
+        cv2.putText(img, str, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
+    elif count_defects == 3:
+        cv2.putText(img,"4", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
+    elif count_defects == 4:
+        cv2.putText(img,"5", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
+    else:
+        cv2.putText(img,"1", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2,(0,0,255), 2)
+    cv2.imshow('Gesture', img)
+    all_img = np.hstack((drawing, crop_img))
+    cv2.imshow('Contours', all_img)
+    k = cv2.waitKey(10)
+    if k == 27:
         break
-
-capture.release()
-cv2.destroyAllWindows()
